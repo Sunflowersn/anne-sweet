b03499a3fa839598c9f7ad5ce8a79c82246fe827
sunflower <morozovsemen94@gmail.com>
2020-10-11 23:05:15 +0300

    Выполнил добавление комментариев.
    Комментарии добавляются, стираютс старые данные. Проблема с расширением блока: наезжает на футер. У нового коммента нет отступа снизую

---
+++ js/comment.js
@@ -0,0 +1,27 @@
+let commentForm = document.querySelector('.comment__form');
+let commentList = document.querySelector('.comment__list');
+let commentText = document.querySelector('.comment__textarea');
+let commentName = document.querySelector('.commentator-name');
+let commentEmail = document.querySelector('.commentator-email');
+
+commentForm.onsubmit = function (evt) {
+  evt.preventDefault();
+  let newComment = document.createElement('li');
+  newComment.classList.add('comment__item');
+  let newAvatar = document.createElement('img');
+  newAvatar.classList.add('comment__avatar');
+  newComment.append(newAvatar);
+  let newName = document.createElement('div');
+  newName.classList.add('comment__name');
+  newName.textContent = commentName.value;
+  newComment.append(newName);
+  let newText = document.createElement('p');
+  newText.classList.add('comment__text');
+  newText.textContent = commentText.value;
+  newComment.append(newText);
+  commentText.value = '';
+  commentName.value = '';
+  commentEmail.value = '';
+  commentList.append(newComment);
+};
+
